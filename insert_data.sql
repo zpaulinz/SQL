7afb12c3b68205049b66afb47841d094854f07c5
Paulina Zabielska <115811258+zpaulinz@users.noreply.github.com>
2025-03-27 22:45:00 +0100

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,23 @@
+-- MySQL
+INSERT INTO users (name, age) VALUES ('Jan Kowalski', 30);
+INSERT INTO users (name, age) Values ('Jan Nowak', 25);
+
+--PostgreSQL
+INSERT INTO users (name, age) VALUES ('Jan Kowalski', 30);
+INSERT INTO users (name, age) Values ('Jan Nowak', 25);
+
+--Oracle
+INSERT INTO users (name, age) VALUES ('Jan Kowalski', 30);
+INSERT INTO users (name, age) Values ('Jan Nowak', 25);
+
+--MS SQL
+INSERT INTO users (name, age) VALUES ('Jan Kowalski', 30);
+INSERT INTO users (name, age) Values ('Jan Nowak', 25);
+
+--SQLite
+INSERT INTO users (name, age) VALUES ('Jan Kowalski', 30);
+INSERT INTO users (name, age) Values ('Jan Nowak', 25);
+
+--MariaDB
+INSERT INTO users (name, age) VALUES ('Jan Kowalski', 30);
+INSERT INTO users (name, age) Values ('Jan Nowak', 25);

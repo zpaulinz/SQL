e07dfe95cbecada78cea60ec064a2724b2642b4f
Paulina Zabielska <115811258+zpaulinz@users.noreply.github.com>
2025-03-30 21:46:51 +0200

    Create update_data.sql

---
+++ update_data.sql
@@ -0,0 +1,29 @@
+-- MySQL
+UPDATE users
+SET name = 'John Updated', age = 31
+WHERE id = 1;
+
+--PostgreSQL
+UPDATE users
+SET name = 'John Updated', age = 31
+WHERE id = 1;
+
+--Oracle
+UPDATE users
+SET name = 'John Updated', age = 31
+WHERE id = 1;
+
+--MS SQL
+UPDATE users
+SET name = 'John Updated', age = 31
+WHERE id = 1;
+
+--SQLite
+UPDATE users
+SET name = 'John Updated', age = 31
+WHERE id = 1;
+
+--MariaDB
+UPDATE users
+SET name = 'John Updated', age = 31
+WHERE id = 1;
